aa96ba40ec1680e7f584f5c9622b44fc26152153
lxb <liuxiaobo666233@gmail.com>
2025-05-02 16:56:46 +0800

    Add IntoIter struct and into_iter method for List to enable ownership transfer and iteration. Implement Iterator trait for IntoIter with unit tests for iteration functionality.

--- src/second.rs
+++ src/second.rs
@@ -8,10 +8,17 @@ type Link<T> = Option<Box<Node<T>>>;
 pub struct List<T> {
     head: Link<T>,
 }
+// 通过元组结构体定义迭代器
+pub struct IntoIter<T>(List<T>);
 
 impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
+    }
+
+    // 将List转换为IntoIter, 发生所有权转移, list不在可用
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
     }
 
     pub fn push(&mut self, elem: T) {
@@ -37,19 +44,14 @@ impl<T> List<T> {
     pub fn peek(&self) -> Option<&T> {
         // 通过as_ref方法获取self.head的引用, 并返回闭包函数执行结果
         // 避免self.head的所有权被转移
-        self.head.as_ref().map(|node| {
-            &node.elem
-        })
+        self.head.as_ref().map(|node| &node.elem)
     }
 
     pub fn peek_mut(&mut self) -> Option<&mut T> {
         // 通过as_mut方法获取self.head的可变引用, 并返回闭包函数执行结果
         // 避免self.head的所有权被转移
-        self.head.as_mut().map(|node| {
-            &mut node.elem
-        })
+        self.head.as_mut().map(|node| &mut node.elem)
     }
-
 }
 
 impl<T> Drop for List<T> {
@@ -63,6 +65,14 @@ impl<T> Drop for List<T> {
             current_link = boxed_node.next.take();
             // 4. 当前节点在这里被自动释放
         }
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        // 通过self.0.pop()获取下一个元素
+        self.0.pop()
     }
 }
 
@@ -98,7 +108,6 @@ mod test {
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
 
-
         // 测试泛型
         let mut list = List::new();
         list.push("hello");
@@ -119,19 +128,33 @@ mod test {
 
     #[test]
     fn peek() {
-    let mut list = List::new();
-    assert_eq!(list.peek(), None);
-    assert_eq!(list.peek_mut(), None);
-    list.push(1); list.push(2); list.push(3);
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+        list.push(1);
+        list.push(2);
+        list.push(3);
 
-    assert_eq!(list.peek(), Some(&3));
-    assert_eq!(list.peek_mut(), Some(&mut 3));
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
 
-    list.peek_mut().map(| value| {
-        *value = 42
-    });
+        list.peek_mut().map(|value| *value = 42);
 
-    assert_eq!(list.peek(), Some(&42));
-    assert_eq!(list.pop(), Some(42));
+        assert_eq!(list.peek(), Some(&42));
+        assert_eq!(list.pop(), Some(42));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
     }
 }
