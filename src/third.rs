c9da43aec9d8271ea5a16c76cab1e0869759e5f0
lxb <liuxiaobo666233@gmail.com>
2025-05-03 09:37:28 +0800

    Add third module for linked list implementation and update lib.rs to include it

---
+++ src/third.rs
@@ -0,0 +1,112 @@
+use std::rc::Rc;
+pub struct List<T> {
+    head: Link<T>,
+}
+
+type Link<T> = Option<Rc<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter {
+            next: self.head.as_deref(),
+        }
+    }
+
+    pub fn prepend(&self, elem: T) -> List<T> {
+        List {
+            head: Some(Rc::new(Node {
+                elem,
+                next: self.head.clone(),
+            })),
+        }
+    }
+
+    pub fn tail(&self) -> List<T> {
+        List {
+            // and_then 对已经是 Option 的值不会再进行包装，避免出现 Option 嵌套
+            // Box 和 Rc 的区别：
+            // Box: 独占所有权，next 字段在解引用后直接访问内部值
+            // Rc: 共享所有权，next 字段即使解引用后仍然保持 Option<Rc<T>> 的结构
+            // 使用 clone() 只增加引用计数，不进行深度克隆
+            head: self.head.as_ref().and_then(|node| node.next.clone()),
+        }
+    }
+
+    pub fn head(&self) -> Option<&T> {
+        // 访问T类型的值,使用map即可
+        self.head.as_ref().map(|node| &node.elem)
+    }
+}
+
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut head = self.head.take();
+        while let Some(node) = head {
+            // try_unwrap 尝试将 Rc<T> 转换为 T, 如果 Rc<T> 只有一个强引用, 则转换成功, 否则返回 Err
+            if let Ok(node) = Rc::try_unwrap(node) {
+                head = node.next;
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.elem
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let list = List::new();
+        assert_eq!(list.head(), None);
+
+        let list = list.prepend(1).prepend(2).prepend(3);
+        assert_eq!(list.head(), Some(&3));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&2));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&1));
+
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let list = List::new().prepend(1).prepend(2).prepend(3);
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+        assert_eq!(iter.next(), None);
+    }
+}
